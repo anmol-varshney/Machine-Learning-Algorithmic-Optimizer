cc667be9b91b8e30a54a3936168bd1a64e6a82ef
Code-with-Palak <palakgupta33939@gmail.com>
2022-01-21 17:13:05 +0530

    path_changes

--- utils/classification_metrics.py
+++ utils/classification_metrics.py
@@ -1,4 +1,4 @@
-BASE_DIR = 'app/Machine Learning Algorithmic Optimizer/'
+BASE_DIR = 'https://github.com/Code-with-Palak/Machine-Learning-Algorithmic-Optimizer/'
 import warnings
 from numpy.lib.function_base import average
 import matplotlib.pyplot as plt
